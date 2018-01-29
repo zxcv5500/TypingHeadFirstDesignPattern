13b55b0bcce7b1f774fcdaa494e5a35ec4ce6048
zxcv5500 <zxcv5500@gmail.com>
2018-01-30 02:07:08 +0900

    simple remote 패턴 완료

--- TypingHeadFirstDesignPattern/src/headfirst/command/simpleremote/RemoteControlTest.java
+++ TypingHeadFirstDesignPattern/src/headfirst/command/simpleremote/RemoteControlTest.java
@@ -5,9 +5,17 @@ public class RemoteControlTest {
 	public static void main(String[] args) {
 		SimpleRemoteControl remote = new SimpleRemoteControl();
 		Light light = new Light();
+		GarageDoor garageDoor = new GarageDoor();
+		
 		LightOnCommand lightOn = new LightOnCommand(light);
+		LightOffCommand lightOff = new LightOffCommand(light);
+		
+		GarageDoorOpenCommand garageDoorOpenUp = new GarageDoorOpenCommand(garageDoor);
+		
 		
 		remote.setCommand(lightOn);
+		remote.buttonWasPressed();
+		remote.setCommand(garageDoorOpenUp);
 		remote.buttonWasPressed();
 	}
 
