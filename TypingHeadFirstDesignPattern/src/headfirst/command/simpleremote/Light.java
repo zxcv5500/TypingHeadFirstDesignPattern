1ab13830dcef7c9c74c4f752e161fe9b1c9596b5
zxcv5500 <zxcv5500@gmail.com>
2018-01-30 01:47:19 +0900

    빛 클래스 수정.

--- TypingHeadFirstDesignPattern/src/headfirst/command/simpleremote/Light.java
+++ TypingHeadFirstDesignPattern/src/headfirst/command/simpleremote/Light.java
@@ -1,19 +1,17 @@
 package headfirst.command.simpleremote;
 
 public class Light {
-	String location = "";
 
 	public Light() {
 		
-	}
-	
+	}	
 	
 	public void on() {
-		System.out.println(location + " light is on");
+		System.out.println(" light is on");
 	}
 	
 	public void off() {
-		System.out.println(location + " light is off");
+		System.out.println(" light is off");
 	}
 	
 }
