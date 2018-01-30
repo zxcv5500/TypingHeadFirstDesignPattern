cc0ed9abf172a8b9866001e4e59563acbe85217f
zxcv5500 <zxcv5500@gmail.com>
2018-01-30 20:37:44 +0900

    Command 패턴 객체들을 활용하는 테스트 코드 파일 추가.

---
+++ TypingHeadFirstDesignPattern/src/headfirst/command/remote/RemoteLoader.java
@@ -0,0 +1,49 @@
+package headfirst.command.remote;
+
+public class RemoteLoader {
+
+	public static void main(String[] args) {
+		RemoteControl remoteControl = new RemoteControl();
+		
+		Light livingRoomLight = new Light("Living Room");
+		Light KitchenLight = new Light("Kitchen");
+		CeilingFan ceilingFan = new CeilingFan("Living Room");
+		GarageDoor garageDoor = new GarageDoor("");
+		Stereo stereo = new Stereo("Living Room");
+		
+		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+		LightOnCommand kitchenLightOn = new LightOnCommand(KitchenLight);
+		LightOffCommand kitchenLightOff  = new LightOffCommand(KitchenLight);
+		
+		CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
+		CeilingFanOffCommand ceilingFanoff = new CeilingFanOffCommand(ceilingFan);
+		
+		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+		GarageDoorDownCommand garageDoorDownCommand = new GarageDoorDownCommand(garageDoor);
+		
+		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+		
+		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+		remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+		remoteControl.setCommand(2, ceilingFanOn, ceilingFanoff);
+		remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+		
+		
+		System.out.println(remoteControl.toString());
+		
+		remoteControl.onButtonWasPushed(0);
+		remoteControl.offButtonWasPushed(0);
+		remoteControl.onButtonWasPushed(1);
+		remoteControl.offButtonWasPushed(1);
+		remoteControl.onButtonWasPushed(2);
+		remoteControl.offButtonWasPushed(2);
+		remoteControl.onButtonWasPushed(3);
+		remoteControl.offButtonWasPushed(3);
+		
+		
+
+	}
+
+}
