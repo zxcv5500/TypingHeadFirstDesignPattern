1e7e3057e9bd8fedb23dd4746d03464de92f7e7c
sharpen <sharpen5500@gmail.com>
2018-02-01 18:54:31 +0900

    실제 동작 테스트 파일 완성.

--- TypingHeadFirstDesignPattern/src/headfirst/command/undo/RemoteLoader.java
+++ TypingHeadFirstDesignPattern/src/headfirst/command/undo/RemoteLoader.java
@@ -5,12 +5,30 @@ public class RemoteLoader {
 	public static void main(String[] args) {
 		RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
 		
+		CeilingFan ceilingFan = new CeilingFan("Living Room");
+		
+		CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
+		CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
+		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+		
 		Light livingRoomLight = new Light("Living Room");
 		
 		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
 		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
 		
 		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+		remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff);
+		remoteControl.setCommand(2, ceilingFanMedium, ceilingFanOff);
+		
+		
+		remoteControl.onButtonWasPushed(1);
+		remoteControl.offButtonWasPushed(1);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		
+		remoteControl.onButtonWasPushed(2);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
 		
 		remoteControl.onButtonWasPushed(0);
 		remoteControl.offButtonWasPushed(0);
