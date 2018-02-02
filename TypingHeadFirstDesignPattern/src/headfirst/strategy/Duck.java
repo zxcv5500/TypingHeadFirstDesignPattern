c761f4a71b7feef7b104b5ee30cc76fa12c310bf
zxcv5500 <zxcv5500@gmail.com>
2018-02-02 23:09:04 +0900

    duck 클래스에 동적으로 변경할 수 있도록 코드 setter 메소드 추가

--- TypingHeadFirstDesignPattern/src/headfirst/strategy/Duck.java
+++ TypingHeadFirstDesignPattern/src/headfirst/strategy/Duck.java
@@ -8,10 +8,20 @@ public abstract class Duck {
 		
 	}
 	
+	
+	public void setFlyBehavior(FlyBehavior fb) {
+		flyBehavior = fb;
+	}
+	
+	public void setQuackBehavior(QuackBehavior gb) {
+		quackBehavior = gb;
+	}
+	
+	
 	public abstract void display();				// 자손 클래스들은 무조건 구현해야하는 abstract 메소드
 	
 	
-	/* interface로 변화되는 부분을 추상화 해낸 interface에 행동을 위임하는 메소드 */
+	/* interface로 변화되는 부분을 추상화 해낸 interface에 행동을 위임하는 메소드들 */
 	public void performFly() {
 		flyBehavior.fly();
 	}
@@ -23,6 +33,8 @@ public abstract class Duck {
 	public void swim() {
 		System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠");
 	}
+	
+
 	
 	
 }
