a4c1c9da1e359c4ef3c51500ea68d2506005d32b
sharpen <sharpen5500@gmail.com>
2018-02-02 19:55:17 +0900

    몰오리 클래스를 작성했다.

---
+++ TypingHeadFirstDesignPattern/src/headfirst/strategy/MallardDuck.java
@@ -0,0 +1,17 @@
+package headfirst.strategy;
+
+public class MallardDuck extends Duck {
+
+	public MallardDuck() {
+		quackBehavior = new Quack();
+		flyBehavior = new FlyWithWings();
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("저는 물오리입니다.");
+	}
+	
+	
+
+}
