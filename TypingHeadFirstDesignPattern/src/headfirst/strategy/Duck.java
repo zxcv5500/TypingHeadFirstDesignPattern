8e52b7b3c88bdc0c6e11802fa413011a4328e955
sharpen <sharpen5500@gmail.com>
2018-02-02 19:47:11 +0900

    오리로 분류되는 것들의 공통점을 뽑아서 추상화해 논 Duck 클래스 작성

---
+++ TypingHeadFirstDesignPattern/src/headfirst/strategy/Duck.java
@@ -0,0 +1,28 @@
+package headfirst.strategy;
+
+public abstract class Duck {
+	FlyBehavior flyBehavior;
+	QuackBehavior quackBehavior;
+	
+	public Duck() {
+		
+	}
+	
+	public abstract void display();				// 자손 클래스들은 무조건 구현해야하는 abstract 메소드
+	
+	
+	/* interface로 변화되는 부분을 추상화 해낸 interface에 행동을 위임하는 메소드 */
+	public void performFly() {
+		flyBehavior.fly();
+	}
+	
+	public void performQuack() {
+		quackBehavior.quack();
+	}
+	
+	public void swim() {
+		System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠");
+	}
+	
+	
+}
