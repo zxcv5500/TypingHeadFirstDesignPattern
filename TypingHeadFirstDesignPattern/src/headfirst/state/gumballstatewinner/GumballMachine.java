f078bf6a68ef6df0591c472641ea1d7bf9736cbc
sharpen <sharpen5500@gmail.com>
2018-02-02 13:53:19 +0900

    당첨확률이 있는 껌 자판기 클래스 작성.

---
+++ TypingHeadFirstDesignPattern/src/headfirst/state/gumballstatewinner/GumballMachine.java
@@ -0,0 +1,79 @@
+package headfirst.state.gumballstatewinner;
+
+public class GumballMachine {
+	
+	State soldOutState;
+	State noQuarterState;
+	State hasQuarterState;
+	State soldState;
+	State winnerState;
+	
+	State state = soldOutState;
+	int count = 0;
+	
+	public GumballMachine(int numberGumballs) {
+		soldOutState = new SoldOutState(this);
+		noQuarterState = new NoQuarterState(this);
+		hasQuarterState = new HasQuarterState(this);
+		winnerState = new WinnerState(this);
+		
+		this.count = numberGumballs;
+		if (numberGumballs > 0) {
+			state = noQuarterState;
+		}
+	}
+	
+	public void insertQuarter() {
+		state.insertQuarter();
+	}
+	
+	public void ejectQuarter() {
+		state.ejectQuarter();
+	}
+	
+	public void turnCrank() {
+		state.turnCrank();
+		state.dispense();
+	}
+	
+	void releaseBall() {
+		System.out.println("껌볼이 슬롯 밖으로 나갔습니다.");
+		if (count != 0) {
+			count = count - 1;
+		}
+	}
+	
+	int getCount() {
+		return count;
+	}
+	
+	void refill(int count) {
+		this.count = count;
+		state = noQuarterState;
+	}
+	
+	public State getState() {
+		return state;
+	}
+
+	public State getSoldOutState() {
+		return soldOutState;
+	}
+
+	public State getNoQuarterState() {
+		return noQuarterState;
+	}
+
+	public State getHasQuarterState() {
+		return hasQuarterState;
+	}
+
+	public State getSoldState() {
+		return soldState;
+	}
+
+	public State getWinnerState() {
+		return winnerState;
+	}
+
+}
