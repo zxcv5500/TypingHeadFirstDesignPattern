1bfc330209baf845e48c89b8e1d3235d4cb61520
zxcv5500 <zxcv5500@gmail.com>
2018-03-16 02:55:02 +0900

    칠면조 부류 인터페이스 스케치

---
+++ TypingHeadFirstDesignPattern/src/headfirst/adapter/ducks/Turkey.java
@@ -0,0 +1,9 @@
+package headfirst.adapter.ducks;
+
+public interface Turkey {
+	
+	public void gobble();			// 칠면조는 꽥꽥 대지 않습니다.
+	
+	public void fly();				// 칠면조도 날 수 있긴 합니다. 별로 멀리 날아갈 수 없긴 하지만요.
+	
+}
