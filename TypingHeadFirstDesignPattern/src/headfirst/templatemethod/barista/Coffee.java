bce28e2eadcf4017e6491ef5449edbe59de01d7c
zxcv5500 <zxcv5500@gmail.com>
2018-03-19 11:33:35 +0900

    커피를 카페인음료 부모 클래스를 상속 받도록 소스 수정

--- TypingHeadFirstDesignPattern/src/headfirst/templatemethod/barista/Coffee.java
+++ TypingHeadFirstDesignPattern/src/headfirst/templatemethod/barista/Coffee.java
@@ -1,31 +1,18 @@
 package headfirst.templatemethod.barista;
 
-public class Coffee {
+public class Coffee extends CaffeineBeverage {
+		
 	
-	void prepareRecipe() {
-		boilWater();
-		brewCoffeeGrinds();
-		pourInCup();
-		addSugarAndMilk();
+	@Override
+	void brew() {
+		System.out.println("필터로 커피를 우려내는 중");
 	}
 	
-	public void boilWater() {
-		System.out.println("물 끓이는 중");
-	}
-
-	
-	private void brewCoffeeGrinds() {
-		System.out.println("필터를 통해서 커피를 우려내는 중");		
-	}
-	
-
-	private void pourInCup() {
-		System.out.println("컵에 따르는 중");		
+	@Override
+	void addCondiments() {
+		System.out.println("설탕과 커피를 추가하는 중");
 	}
 
 	
-	private void addSugarAndMilk() {
-		System.out.println("설탕과 우유를 추가하는 중");		
-	}
 	
 }
