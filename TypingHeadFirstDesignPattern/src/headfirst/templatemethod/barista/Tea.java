6f93842e5fa377fadb8dbd64223fe0a51652f5c5
zxcv5500 <zxcv5500@gmail.com>
2018-03-19 11:35:27 +0900

    티 클래스를 카페인음료 부모 클래스의 상속을 받도록 소스 수정

--- TypingHeadFirstDesignPattern/src/headfirst/templatemethod/barista/Tea.java
+++ TypingHeadFirstDesignPattern/src/headfirst/templatemethod/barista/Tea.java
@@ -1,26 +1,15 @@
 package headfirst.templatemethod.barista;
 
-public class Tea {
-	void prepareRecipe() {
-		boilWater();
-		steepTeaBag();
-		pourInCup();
-		addLemon();
+public class Tea extends CaffeineBeverage {
+
+	@Override
+	void brew() {
+		System.out.println("차를 우려내는 중");		
 	}
 
-	private void boilWater() {
-		System.out.println("물 끓이는 중");		
+	@Override
+	void addCondiments() {
+		System.out.println("설탕과 커피를 추가하는 중");		
 	}
-
-	private void steepTeaBag() {
-		System.out.println("차를 우려내는 중");
-	}
-
-	private void pourInCup() {
-		System.out.println("레몬을 추가하는 중");		
-	}
-
-	private void addLemon() {
-		System.out.println("컵에 따르는 중");		
-	}
+	
 }
