b128deaba160f1bec9c3acf7f13bcb36f6c71e83
zxcv5500 <zxcv5500@gmail.com>
2018-03-19 15:56:59 +0900

    웹에서 동작하는 간단한 애플릿 프로그램 (템플릿 메소드 패턴 예제)

---
+++ TypingHeadFirstDesignPattern/src/headfirst/templatemethod/applet/MyApplet.java
@@ -0,0 +1,31 @@
+package headfirst.templatemethod.applet;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class MyApplet extends Applet {
+	String message;
+	
+	public void init() {
+		message = "안녕하세요. 제가 왔어요...";
+		repaint();
+	}
+	
+	public void start() {
+		message = "시작 중...";
+		repaint();
+	}
+	
+	public void stop() {
+		message = "저를 떠나려 하시는 군요...";
+		repaint();
+	}
+	
+	public void destory() {
+		// 애플릿이 제거됨...
+	}
+	
+	public void paint(Graphics g) {
+		g.drawString(message, 5, 15);
+	}
+}
