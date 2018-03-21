47c98fa246191ab05b65e98d85e373ca54b35891
zxcv5500 <zxcv5500@gmail.com>
2018-03-21 13:33:33 +0900

    캐릭터 부모클래스 정의 작성

--- TypingHeadFirstDesignPattern/src/headfirst/strategy/designpuzzle/Character.java
+++ TypingHeadFirstDesignPattern/src/headfirst/strategy/designpuzzle/Character.java
@@ -1,12 +1,14 @@
 package headfirst.strategy.designpuzzle;
 
 public abstract class Character {
+	
 	WeaponBehavior weaponBehavior;
-
+	
 	public Character(WeaponBehavior weaponBehavior) {
+		super();
 		this.weaponBehavior = weaponBehavior;
-		
 	}
+
 
 	public void setWeapon(WeaponBehavior weaponBehavior) {
 		this.weaponBehavior = weaponBehavior;
