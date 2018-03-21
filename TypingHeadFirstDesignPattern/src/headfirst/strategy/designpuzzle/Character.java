bb45b8c4acd6cb431745c645370e8ad67d3ed675
zxcv5500 <zxcv5500@gmail.com>
2018-03-21 13:04:45 +0900

    Character 부모 클래스 추가

---
+++ TypingHeadFirstDesignPattern/src/headfirst/strategy/designpuzzle/Character.java
@@ -0,0 +1,22 @@
+package headfirst.strategy.designpuzzle;
+
+public abstract class Character {
+	WeaponBehavior weaponBehavior;
+
+	public Character(WeaponBehavior weaponBehavior) {
+		this.weaponBehavior = weaponBehavior;
+		
+	}
+
+	public void setWeapon(WeaponBehavior weaponBehavior) {
+		this.weaponBehavior = weaponBehavior;
+	}
+	
+	
+	public void performUseWeapon() {
+		weaponBehavior.useWeapon();
+	}
+	
+	public abstract void fight();
+
+}
