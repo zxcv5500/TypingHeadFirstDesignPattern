e3c79c13a511bd097c91e4f066d5f79b76382b57
sharpen <sharpen5500@gmail.com>
2018-03-26 12:49:40 +0900

    현재 정보 디스플레이 코드 작성. DisplayElement 인터페이스는 이전 패키지에서 복사해서 갖다 붙여서씀

---
+++ TypingHeadFirstDesignPattern/src/headfirst/observer/weatherobservable/DisplayElement.java
@@ -0,0 +1,9 @@
+package headfirst.observer.weatherobservable;
+
+/**
+ * @author zxcv5500
+ * 이 인터페이스는 display()메소드 하나 밖에 없습니다. 디스플레이 항목을 표시해야 하는 경우 그 메소드를 호출하면 됩니다.
+ */
+public interface DisplayElement {
+	public void display();
+}
