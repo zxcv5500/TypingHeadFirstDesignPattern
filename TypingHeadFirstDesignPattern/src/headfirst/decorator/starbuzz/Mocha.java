7e3b9148f15e3ec598266396a3cc30d743585222
sharpen <sharpen5500@gmail.com>
2018-03-26 20:18:50 +0900

    첨가물_모카 클래스 작성

---
+++ TypingHeadFirstDesignPattern/src/headfirst/decorator/starbuzz/Mocha.java
@@ -0,0 +1,24 @@
+package headfirst.decorator.starbuzz;
+
+public class Mocha extends CondimentDecorator {
+	Beverage beverage;
+
+	
+	
+	public Mocha(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	@Override
+	public String getDescription() {
+		
+		return beverage.getDescription() + ", 모카";
+	}
+
+	@Override
+	public double cost() {
+		
+		return .20 + beverage.cost();
+	}
+
+}
