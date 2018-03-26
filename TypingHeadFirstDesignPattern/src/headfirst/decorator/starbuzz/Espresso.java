4c4af46bf8f441aa85e85038ab19dcd2768d99e4
sharpen <sharpen5500@gmail.com>
2018-03-26 19:54:33 +0900

    음료수의 자식클래스인 에스프레소 클래스 작성

---
+++ TypingHeadFirstDesignPattern/src/headfirst/decorator/starbuzz/Espresso.java
@@ -0,0 +1,15 @@
+package headfirst.decorator.starbuzz;
+
+public class Espresso extends Beverage {
+	
+	public Espresso() {
+		description = "에스프레소";
+	}
+
+	@Override
+	public double cost() {
+		
+		return 1.99;
+	}
+
+}
