b00ccd8f569a27a297bfd256319d5b974d8d0577
sharpen <sharpen5500@gmail.com>
2018-03-27 15:06:56 +0900

    피자 가게에서 공장을 통해 피자를 생성함. 준비 굽기 자르기 포장하는 기능을 포함한 클래스 작성함

---
+++ TypingHeadFirstDesignPattern/src/headfirst/factory/pizzas/PizzaStore.java
@@ -0,0 +1,25 @@
+package headfirst.factory.pizzas;
+
+public class PizzaStore {
+	SimplePizzaFactory factory;
+
+	public PizzaStore(SimplePizzaFactory factory) {
+		super();
+		this.factory = factory;
+	}
+	
+	public Pizza orderPizza(String type) {
+		Pizza pizza;
+		
+		pizza = factory.createPizza(type);
+		
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+		
+		return pizza;
+	}
+	
+	
+}
