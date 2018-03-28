7405e01db4e35a5341193928a485e5eb7b803646
sharpen <sharpen5500@gmail.com>
2018-03-28 09:53:11 +0900

    시카고풍 피자 가게 클래스를 작성함.시카고풍 피자 클래스들은 이후 작성 예정

---
+++ TypingHeadFirstDesignPattern/src/headfirst/factory/pizzafm/ChicagoPizzaStore.java
@@ -0,0 +1,18 @@
+package headfirst.factory.pizzafm;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+	@Override
+	Pizza createPizza(String item) {
+		if ("cheese".equals(item)) {
+			return new ChicagoStyleCheesePizza();
+		} else if ("veggie".equals(item)) {
+			return new ChicagoStyleVeggiePizza();
+		} else if ("clam".equals(item)) {
+			return new ChicagoStyleClamPizza();
+		} else if ("pepperoni".equals(item)) {
+			return new ChicagoStylePepperoniPizza();
+		} else return null;
+	}
+
+}
