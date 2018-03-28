5efdd97ff290be6ad007112b239774240bae7397
sharpen <sharpen5500@gmail.com>
2018-03-28 10:20:35 +0900

    피자 가게에 주문을 시키는 실행테스트 코드 작성

--- TypingHeadFirstDesignPattern/src/headfirst/factory/pizzafm/PizzaTestDrive.java
+++ TypingHeadFirstDesignPattern/src/headfirst/factory/pizzafm/PizzaTestDrive.java
@@ -4,12 +4,33 @@ public class PizzaTestDrive {
 
 	public static void main(String[] args) {
 		PizzaStore nyStore = new NyPizzaStore();
+		PizzaStore chicagoStore = new ChicagoPizzaStore();
 		
 		Pizza pizza = nyStore.orderPizza("cheese");
 		System.out.println("Ethan ordered a " + pizza.getName() + "\n");
 		
-//		pizza = chicagoStore.orderPizza("cheese");
-//		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		pizza = chicagoStore.orderPizza("cheese");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		
+		pizza = chicagoStore.orderPizza("clam");
+		System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+		
+		pizza = chicagoStore.orderPizza("clam");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		
+		pizza = chicagoStore.orderPizza("pepperoni");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		
+		pizza = chicagoStore.orderPizza("pepperoni");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		
+		pizza = chicagoStore.orderPizza("veggie");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		
+		pizza = chicagoStore.orderPizza("veggie");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		
+		
 	}
 
 }
