0ff1ff8f9576ba1ad52fdadf9beefd4319fccb25
sharpen <sharpen5500@gmail.com>
2018-03-29 15:46:47 +0900

    추상 팩토리 패턴 실행 테스트  코드 수정

--- TypingHeadFirstDesignPattern/src/headfirst/factory/pizzaaf/PizzaTestDrive.java
+++ TypingHeadFirstDesignPattern/src/headfirst/factory/pizzaaf/PizzaTestDrive.java
@@ -9,6 +9,29 @@ public class PizzaTestDrive {
 		Pizza pizza = nyStore.orderPizza("cheese");
 		System.out.println("Ethan ordered a " + pizza + "\n");
 
+		pizza = chicagoStore.orderPizza("cheese");
+		System.out.println("Joel ordered a " + pizza + "\n");
+		
+		pizza = nyStore.orderPizza("clam");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+		
+		pizza = chicagoStore.orderPizza("clam");
+		System.out.println("Joel ordered a " + pizza + "\n");
+		
+		pizza = nyStore.orderPizza("pepperoni");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+		
+		pizza = chicagoStore.orderPizza("pepperoni");
+		System.out.println("Joel ordered a " + pizza + "\n");
+		
+		pizza = nyStore.orderPizza("veggie");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+		
+		pizza = chicagoStore.orderPizza("veggie");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+		
+		
+		
 	}
 
 }
