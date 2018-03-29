7fed13505fa1b25558834e7b30549fee3e90f89b
sharpen <sharpen5500@gmail.com>
2018-03-29 09:37:38 +0900

    패키지 약어 보충 설명을 추가함

--- TypingHeadFirstDesignPattern/src/headfirst/factory/pizzaaf/package-info.java
+++ TypingHeadFirstDesignPattern/src/headfirst/factory/pizzaaf/package-info.java
@@ -3,6 +3,6 @@
  */
 /**
  * @author sharpen
- * 추상 팩토리 패턴에 대해 학습합니다. 팩토리 메소드x 패턴이 아닙니다. 
+ * 추상 팩토리(Abstract Factory -> af )  패턴에 대해 학습합니다. 팩토리 메소드x 패턴이 아닙니다. 
  */
 package headfirst.factory.pizzaaf;
