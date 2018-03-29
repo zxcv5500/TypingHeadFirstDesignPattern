c3e178975e293baef8eb6d7a996f46bb67f3b8b4
zxcv5500 <zxcv5500@gmail.com>
2018-03-30 01:32:39 +0900

    메뉴 구성 요소는 잎 노드와 복합노드 모두에서 쓰이는 인터페이스 역할을 함.

---
+++ TypingHeadFirstDesignPattern/src/headfirst/composite/menu/MenuComponent.java
@@ -0,0 +1,40 @@
+package headfirst.composite.menu;
+
+/**
+ * @author zxcv5500
+ * 메뉴구성 요소는 잎노드와 복합노드 모두에서 쓰이는 인터페이스 역할을 한다.
+ */
+public abstract class MenuComponent {
+	public void add(MenuComponent menuComponent) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void remove(MenuComponent menuComponent) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public MenuComponent getChild(int i) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getName() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getDescription() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getPrice() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean isVegetarian() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void print() {
+		throw new UnsupportedOperationException();
+	}
+	
+}
