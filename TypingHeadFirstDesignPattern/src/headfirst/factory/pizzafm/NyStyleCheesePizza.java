66e1ceb1ac7702bf1847b6d9d5c7af773cfef8d9
sharpen <sharpen5500@gmail.com>
2018-03-29 13:21:54 +0900

    뉴욕 스타일 치즈 피자 코드 누락 수정

--- TypingHeadFirstDesignPattern/src/headfirst/factory/pizzafm/NyStyleCheesePizza.java
+++ TypingHeadFirstDesignPattern/src/headfirst/factory/pizzafm/NyStyleCheesePizza.java
@@ -1,5 +1,10 @@
 package headfirst.factory.pizzafm;
 
 public class NyStyleCheesePizza extends Pizza {
-	
+	public NyStyleCheesePizza() {
+		dough = "NY Style Sauce and Cheese Pizza";
+		name = "Thin Crust Dough";
+		sauce = "Marinara Sauce";
+		toppings.add("Grated Reggiano Cheese");		
+	}
 }
