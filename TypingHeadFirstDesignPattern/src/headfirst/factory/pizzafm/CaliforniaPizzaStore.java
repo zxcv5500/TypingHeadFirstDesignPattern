5f416f621f68334cd37ced1d16956ae5c01b782c
sharpen <sharpen5500@gmail.com>
2018-03-29 13:45:29 +0900

    캘리포니아 피자 가게 클래스 작성 (복습 차원)

---
+++ TypingHeadFirstDesignPattern/src/headfirst/factory/pizzafm/CaliforniaPizzaStore.java
@@ -0,0 +1,20 @@
+package headfirst.factory.pizzafm;
+
+public class CaliforniaPizzaStore extends PizzaStore {
+
+	@Override
+	Pizza createPizza(String item) {
+		
+		if ("cheese".equals(item)) {
+			return new CaliforniaCheesePizza();
+		} else if ("clam".equals(item)) {
+			return new CaliforniaClamPizza();
+		} else if ("veggie".equals(item)) {
+			return new CaliforniaVeggiePizza();
+		} else if ("pepperoni".equals(item)) {
+			return new CaliforniaPepperoniPizza();
+		}
+		
+	}
+
+}
