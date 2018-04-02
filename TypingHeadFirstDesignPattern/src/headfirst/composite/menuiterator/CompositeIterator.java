33e96deee44e0087994c98eb37a978b3e54f8180
zxcv5500 <zxcv5500@gmail.com>
2018-04-02 18:38:40 +0900

    컴포지트 패턴에 iterator패턴을 추가한 예제 코드 적용.

---
+++ TypingHeadFirstDesignPattern/src/headfirst/composite/menuiterator/CompositeIterator.java
@@ -0,0 +1,46 @@
+package headfirst.composite.menuiterator;
+
+import java.util.*;
+
+public class CompositeIterator implements Iterator {
+	Stack stack = new Stack();
+	
+	public CompositeIterator(Iterator iterator) {
+		stack.push(iterator);
+	}
+
+	@Override
+	public Object next() {
+		if (hasNext()) {
+			Iterator iterator = (Iterator) stack.peek();
+			MenuComponent component = (MenuComponent) iterator.next();
+			if (component instanceof Menu) {
+				stack.push(component.createIterator());
+			}
+			return component;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (stack.empty()) {
+			return false;
+		} else {
+			Iterator iterator = (Iterator) stack.peek();
+			if (!iterator.hasNext()) {
+				stack.pop();
+				return hasNext();
+			} else {
+				return true;
+			}
+		}
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+		
+}
---
+++ TypingHeadFirstDesignPattern/src/headfirst/composite/menuiterator/Menu.java
@@ -0,0 +1,78 @@
+package headfirst.composite.menuiterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Menu extends MenuComponent {
+	ArrayList menuComponents = new ArrayList<>();
+	String name;
+	String description;
+	
+	
+	
+	public Menu(String name, String description) {
+		super();
+		this.name = name;
+		this.description = description;
+	}
+
+
+
+	@Override
+	public void add(MenuComponent menuComponent) {
+		menuComponents.add(menuComponent);
+	}
+
+
+
+	@Override
+	public void remove(MenuComponent menuComponent) {
+		menuComponents.remove(menuComponent);
+	}
+
+	
+	@Override
+	public MenuComponent getChild(int i) {
+		
+		return (MenuComponent) menuComponents.get(i);
+	}
+
+
+
+	@Override
+	public String getName() {
+		
+		return name;
+	}
+
+
+
+	@Override
+	public String getDescription() {
+		
+		return description;
+	}
+
+
+
+	@Override
+	public Iterator createIterator() {
+		
+		return new CompositeIterator(menuComponents.iterator());
+	}
+	
+	@Override
+	public void print() {
+		System.out.println("\n" + getName());
+		System.out.println(", " + getDescription());
+		System.out.println("------------------------");
+		
+		Iterator iterator = menuComponents.iterator();
+		while (iterator.hasNext()) {
+			MenuComponent menuComponent = (MenuComponent) iterator.next();
+			menuComponent.print();
+		}
+		
+	}
+
+}
