71977b66b1e428eb9357dbb60107c6ac5a2b2d7a
zxcv5500 <zxcv5500@gmail.com>
2018-04-03 01:30:31 +0900

    오리 시뮬레이터 작성

---
+++ TypingHeadFirstDesignPattern/src/headfirst/compound/ducks/DuckSimulator.java
@@ -0,0 +1,29 @@
+package headfirst.compound.ducks;
+
+public class DuckSimulator {
+
+	public static void main(String[] args) {
+		DuckSimulator simulator = new DuckSimulator();
+		simulator.simulate();
+	}
+	
+	void simulate() {
+		Quackable mallardDuck = new MallardDuck();
+		Quackable redheadDuck = new RedHeadDuck();
+		Quackable duckCall = new DuckCall();
+		Quackable rubberDuck = new RubberDuck();
+		
+		System.out.println("\nDuck Simulator");
+		
+		simulate(mallardDuck);
+		simulate(redheadDuck);
+		simulate(duckCall);
+		simulate(rubberDuck);
+		
+	}
+
+	void simulate(Quackable duck) {
+		duck.quack();
+	}
+
+}
