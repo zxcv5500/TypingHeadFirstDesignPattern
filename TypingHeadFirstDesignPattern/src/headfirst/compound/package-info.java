21bb70db4176c9c740cb8bbecc8459efffe5010d
zxcv5500 <zxcv5500@gmail.com>
2018-04-03 16:58:22 +0900

    compound 학습 순서 정리하여 패키지 설명에 추가함

--- TypingHeadFirstDesignPattern/src/headfirst/compound/package-info.java
+++ TypingHeadFirstDesignPattern/src/headfirst/compound/package-info.java
@@ -5,6 +5,6 @@
  * @author zxcv5500
  * Compound 패턴 패키지 생성함
  * 패키지 별 학습 진행 순서
- * ducks -> adapter -> decorator -> factory ->
+ * ducks -> adapter -> decorator -> factory -> composite -> observer
  */
 package headfirst.compound;
