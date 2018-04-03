47a5e69eef43321743f2dca09bc075df0f663267
zxcv5500 <zxcv5500@gmail.com>
2018-04-03 09:00:35 +0900

    유인용 오리 누락 클래스 코드 추가작성

---
+++ TypingHeadFirstDesignPattern/src/headfirst/compound/ducks/DecoyDuck.java
@@ -0,0 +1,14 @@
+package headfirst.compound.ducks;
+
+/**
+ * @author zxcv5500
+ * 유인용 오리 클래스 작성
+ */
+public class DecoyDuck implements Quackable {
+
+	@Override
+	public void quack() {
+		System.out.println("<< Silence >>");
+	}
+
+}
