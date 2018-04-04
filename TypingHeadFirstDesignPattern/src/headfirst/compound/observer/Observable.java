2d5c50a59213eafc0ddcf6abab48ad6c4f9807aa
zxcv5500 <zxcv5500@gmail.com>
2018-04-04 23:18:45 +0900

    Observable 클래스 작성

---
+++ TypingHeadFirstDesignPattern/src/headfirst/compound/observer/Observable.java
@@ -0,0 +1,37 @@
+package headfirst.compound.observer;
+
+import java.util.ArrayList;
+
+import headfirst.iterator.dinermerger.Iterator;
+
+public class Observable implements QuackObservable {
+	ArrayList observers = new ArrayList<>();
+	QuackObservable duck;
+
+	public Observable(QuackObservable duck) {
+		this.duck = duck;
+	};
+	
+	
+
+	@Override
+	public void registerObserver(Observer observer) {
+		observers.add(observer);		
+	}
+	
+	@Override
+	public void notifyObservers() {
+		Iterator iterator = observers.iterator();
+		while (iterator.hasNext()) {
+			Observer observer = (Observer) iterator.next();
+			observer.update(duck);
+		}
+	}
+	
+	
+	public Iterator getObservers() {
+		return observers.iterator();
+	}
+	
+
+}
