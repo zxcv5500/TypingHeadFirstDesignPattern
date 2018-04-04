a21b666f17f50cd5b728b95be0f463382d438b05
zxcv5500 <zxcv5500@gmail.com>
2018-04-04 23:19:03 +0900

    구체적인 구상 클래스 작성

--- TypingHeadFirstDesignPattern/src/headfirst/compound/observer/MallardDuck.java
+++ TypingHeadFirstDesignPattern/src/headfirst/compound/observer/MallardDuck.java
@@ -5,10 +5,31 @@ package headfirst.compound.observer;
  * 표준적인 물오리 클래스
  */
 public class MallardDuck implements Quackable {
-
+	Observable observable;
+	
+	public MallardDuck() {
+		observable = new Observable(this);
+	}
+	
 	@Override
 	public void quack() {
 		System.out.println("Quack");
+		notifyObservers();
+	}
+
+	@Override
+	public void registerObserver(Observer observer) {
+		observable.registerObserver(observer);		
+	}
+
+	@Override
+	public void notifyObservers() {
+		observable.notifyObservers();
+	}
+	
+	@Override
+	public String toString() {
+		return "Mallard Duck";
 	}
 
 }
