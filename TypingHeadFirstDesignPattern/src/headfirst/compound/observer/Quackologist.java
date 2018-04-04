d1abcafa89e26c430329448f66b86676424256e0
zxcv5500 <zxcv5500@gmail.com>
2018-04-04 23:23:11 +0900

    꽥학자들 클래스 작성

---
+++ TypingHeadFirstDesignPattern/src/headfirst/compound/observer/Quackologist.java
@@ -0,0 +1,10 @@
+package headfirst.compound.observer;
+
+public class Quackologist implements Observer {
+
+	@Override
+	public void update(QuackObservable duck) {
+		System.out.println("Quackologist : " + duck + " just quacked");
+	}
+
+}
